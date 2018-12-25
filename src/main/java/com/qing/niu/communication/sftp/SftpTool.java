1f062ceff9111014b69ce920e1a22b1bd140716c
Alan_gui <2516394328@qq.com>
2018-12-25 22:27:32 +0800

    修改

--- src/main/java/com/qing/niu/communication/sftp/SftpTool.java
+++ src/main/java/com/qing/niu/communication/sftp/SftpTool.java
@@ -214,18 +214,18 @@ public class SftpTool {
 
     public static void main(String[] args) throws Exception{
         //密码认证方式
-        SftpTool sftpTool = new SftpTool("192.168.67.144","sftpuser",22);
+        SftpTool sftpTool = new SftpTool("192.168.79.151","albert",22);
         //下载文件
-        File saveFile = new File("/data/sftp/download.txt");
-        sftpTool.download("/admin","1900008721All2018-04-03.csv",saveFile,sftpTool.new AuthTypeMode(AuthTypeEnum.PASSWORD.getCode(),"alan123456"));
+        File saveFile = new File("/data/sftp/verifyfile_01.txt");
+        sftpTool.download("/verifyfile","verifyfile_01.csv",saveFile,sftpTool.new AuthTypeMode(AuthTypeEnum.PASSWORD.getCode(),"alan123456"));
         //上传文件
-        sftpTool.upload("/admin","/data/sftp/download.txt",sftpTool.new AuthTypeMode(AuthTypeEnum.PASSWORD.getCode(),"alan123456"));
+        sftpTool.upload("/verifyfile","/data/sftp/verifyfile_01.txt",sftpTool.new AuthTypeMode(AuthTypeEnum.PASSWORD.getCode(),"alan123456"));
 
         //密钥认证方式下载文件
-        SftpTool sftpToolTwo = new SftpTool("192.168.67.144","qingniu",22);
-        File saveFileTwo = new File("/data/sftp/download.txt");
-        sftpToolTwo.download("/admin","1900008721All2018-04-03.csv",saveFileTwo,sftpToolTwo.new AuthTypeMode(AuthTypeEnum.RSA.getCode(),"/data/rsa/id_rsa_qingniu"));
+        SftpTool sftpToolTwo = new SftpTool("192.168.79.151","qingniu",22);
+        File saveFileTwo = new File("/data/sftp/verifyfile_01.txt");
+        sftpToolTwo.download("/verifyfile","verifyfile_01.csv",saveFileTwo,sftpToolTwo.new AuthTypeMode(AuthTypeEnum.RSA.getCode(),"/data/rsa/id_rsa_qingniu"));
         //密钥认证方式上传文件( 上传会失败Permission denied，因为qingniu这个用户没有写的权限 )
-        sftpToolTwo.upload("/admin","/data/sftp/download.txt",sftpToolTwo.new AuthTypeMode(AuthTypeEnum.RSA.getCode(),"/data/rsa/id_rsa_qingniu"));
+        sftpToolTwo.upload("/verifyfile","/data/sftp/verifyfile_01.txt",sftpToolTwo.new AuthTypeMode(AuthTypeEnum.RSA.getCode(),"/data/rsa/id_rsa_qingniu"));
     }
 }
