d1957f580cd0445bbccd8a02d3e56d2fcb84ba89
alangui <2516394328@qq.com>
2019-05-24 15:56:27 +0800

    修改

---
+++ workstation-spring/src/main/java/com/qing/niu/workstation/spring/ScheduledTask.java
@@ -0,0 +1,15 @@
+package com.qing.niu.workstation.spring;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author huqingniu
+ * @version 1.0.0
+ * @date 2019/5/24
+ */
+@Component
+public class ScheduledTask {
+}
