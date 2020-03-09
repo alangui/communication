a3cbcd46ae9d8d0c8692b87b870e2ac36a90fd7c
Alan_gui <2516394328@qq.com>
2020-03-09 12:23:48 +0800

    update

---
+++ workstation-spring/src/main/java/com/qing/niu/workstation/spring/springboot/Main.java
@@ -0,0 +1,13 @@
+package com.qing.niu.workstation.spring.springboot;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @Author Gui Jin Kang
+ * @Date 2020/3/9 12:23
+ * @ProjectName communication
+ * @Version 1.0.0
+ */
+public class Main {
+}
