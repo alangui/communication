a5a09dafe185c691c61ef9b32a619278829ee460
jkgui <jkgui@easipay.net>
2020-06-28 11:18:51 +0800

    修改

---
+++ workstation-web/src/main/java/com/qing/niu/workstation/web/jdk/jvm/memory/MemoryTest2.java
@@ -0,0 +1,17 @@
+package com.qing.niu.workstation.web.jdk.jvm.memory;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @Author Gui Jin Kang
+ * @Date 2020/6/24 18:43
+ * @ProjectName communication
+ * @Version 1.0.0
+ */
+public class MemoryTest2 {
+
+    public static void main(String[] args) {
+
+    }
+}
