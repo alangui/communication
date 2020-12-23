0004fa20d592a2b3bda193386801dbde5dbdd0b8
guijinkang <guijinkang@dongfangfuli.com>
2020-12-23 16:43:46 +0800

    feat: 工厂模式

---
+++ workstation-design-pattern/src/main/java/com/qing/niu/workstation/design/pattern/factory/Product.java
@@ -0,0 +1,18 @@
+package com.qing.niu.workstation.design.pattern.factory;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @Author Gui Jin Kang
+ * @Date 2020/12/23
+ * @ProjectName communication
+ * @Version 1.0.0
+ */
+public interface Product {
+
+    /**
+     * method
+     */
+    void method();
+}
---
+++ workstation-design-pattern/src/main/java/com/qing/niu/workstation/design/pattern/factory/ProductA.java
@@ -0,0 +1,18 @@
+package com.qing.niu.workstation.design.pattern.factory;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @Author Gui Jin Kang
+ * @Date 2020/12/23
+ * @ProjectName communication
+ * @Version 1.0.0
+ */
+public class ProductA implements Product{
+
+    @Override
+    public void method() {
+        System.out.println("productA method");
+    }
+}
