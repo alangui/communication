c657f68c230a4dac3b3f44ea077037f32be1c59d
guijinkang <guijinkang@dongfangfuli.com>
2020-12-28 15:58:47 +0800

    feat: 装饰模式

---
+++ workstation-design-pattern/src/main/java/com/qing/niu/workstation/design/pattern/decorator/Component.java
@@ -0,0 +1,16 @@
+package com.qing.niu.workstation.design.pattern.decorator;
+
+/**
+ * <p>
+ *     抽象构件角色
+ * </p>
+ *
+ * @Author Gui Jin Kang
+ * @Date 2020/12/28
+ * @ProjectName communication
+ * @Version 1.0.0
+ */
+public interface Component {
+
+    void operation();
+}
